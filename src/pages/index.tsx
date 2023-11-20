209d75894e083ae1199ce3df53d58ca4a6ca852e
wasuken <wevorence@gmail.com>
2023-11-21 00:13:09 +0900

    文言の修正。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ const autoScroll = () => {
 export default function Home() {
   const [articles, setArticles] = useState<Article[]>([]);
   const [hasMore, setHasMore] = useState(true);
+  const [interv, setInterv] = useState(null);
   const loader = (
     <div className={styles.loader} key={0}>
       Loading ...
@@ -38,25 +39,50 @@ export default function Home() {
     }
     setArticles([...articles, ...data]);
   };
-  useEffect(() => {
-    const interval = setInterval(autoScroll, scrollInterval);
+  const handlePauseButtonClick = () => {
+    if (interv) {
+      clearInterval(interv);
+      setInterv(null);
+    } else {
+      const inv = setInterval(autoScroll, scrollInterval);
+      setInterv(inv);
+    }
+  };
 
-    return () => clearInterval(interval);
+  useEffect(() => {
+    const inv = setInterval(autoScroll, scrollInterval);
+    setInterv(inv);
+
+    return () => clearInterval(inv);
   }, []);
   return (
     <div className={styles.main}>
-      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} loader={loader}>
-        <div>
-          {articles.map((article, index) => (
-            <a href={article.link} className={styles.newsLink} target="_blank">
-              <div key={index} className={styles.newsItem}>
-                <h2 className={styles.newsTitle}>{article.title}</h2>
-                <p className={styles.newsDescription}>{article.description}</p>
-              </div>
-            </a>
-          ))}
-        </div>
-      </InfiniteScroll>
+      <div className={styles.sidebar}>
+        <h2>sidebar</h2>
+        <button className={styles.linkButton} onClick={handlePauseButtonClick}>
+          {interv === null ? "restart" : "pause"}
+        </button>
+      </div>
+      <div className={styles.articleArea}>
+        <InfiniteScroll loadMore={loadMore} hasMore={hasMore} loader={loader}>
+          <div>
+            {articles.map((article, index) => (
+              <a
+                href={article.link}
+                className={styles.newsLink}
+                target="_blank"
+              >
+                <div key={index} className={styles.newsItem}>
+                  <h2 className={styles.newsTitle}>{article.title}</h2>
+                  <p className={styles.newsDescription}>
+                    {article.description}
+                  </p>
+                </div>
+              </a>
+            ))}
+          </div>
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
