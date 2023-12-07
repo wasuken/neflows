dec5867f3e3a84abde8742db55aec9ac4029666b
wasuken <wevorence@gmail.com>
2023-12-07 19:12:33 +0900

    added testing.

--- tests/lib/RSSRequestCache.test.ts
+++ tests/lib/RSSRequestCache.test.ts
@@ -1,13 +1,14 @@
 import RSSRequestCache from "../../src/lib/RSSRequestCache";
 import RSSParserWrapper from "../../src/lib/RSSParserWrapper";
 import { Article } from "../../src/const";
+import { setTimeout } from "timers/promises";
 
 import { createServer } from "http";
 import fs from "fs";
 
 describe("RSSRequestCache", () => {
   const ttl = 1000; // 1秒後に期限切れになるように設定
-  it("should use the RSSParserWrapper to fetch data", async () => {
+  it("一件取得のみ", async () => {
     const expected = {
       items: [
         {
@@ -29,6 +30,84 @@ describe("RSSRequestCache", () => {
         title: "test",
         description: "test desc",
         link: "http://example.com",
+      },
+    ]);
+  });
+  it("一件取得後、キャッシングできてるかまで確認", async () => {
+    const beforeData = {
+      items: [
+        {
+          title: "test",
+          contentSnippet: "test desc",
+          link: "http://example.com",
+        },
+      ],
+    };
+    const expected = {
+      items: [
+        {
+          title: "test after",
+          contentSnippet: "test desc after",
+          link: "http://example.com/after",
+        },
+      ],
+    };
+    const mockRSSParser = new RSSParserWrapper();
+    mockRSSParser.parseURL = jest.fn().mockResolvedValue(beforeData);
+
+    const cache = new RSSRequestCache(1000, mockRSSParser);
+    const url = "http://example.com/after";
+    const data = await cache.request(url);
+
+    mockRSSParser.parseURL = jest.fn().mockResolvedValue(expected);
+    const data2 = await cache.request(url);
+
+    // キャッシュから返却しているなら、あたらしいparseURLはつかわないはず。
+    expect(data2).toEqual([
+      {
+        title: "test",
+        description: "test desc",
+        link: "http://example.com",
+      },
+    ]);
+  });
+  it("一件取得後、キャッシュ期限切れてきえた後にキャッシングしていないことを確認", async () => {
+    const beforeData = {
+      items: [
+        {
+          title: "test",
+          contentSnippet: "test desc",
+          link: "http://example.com",
+        },
+      ],
+    };
+    const expected = {
+      items: [
+        {
+          title: "test after",
+          contentSnippet: "test desc after",
+          link: "http://example.com/after",
+        },
+      ],
+    };
+    const mockRSSParser = new RSSParserWrapper();
+    mockRSSParser.parseURL = jest.fn().mockResolvedValue(beforeData);
+
+    const cache = new RSSRequestCache(1000, mockRSSParser);
+    const url = "http://example.com/after";
+    const data = await cache.request(url);
+
+    await setTimeout(1000);
+
+    mockRSSParser.parseURL = jest.fn().mockResolvedValue(expected);
+    const data2 = await cache.request(url);
+
+    // キャッシュから返却していなければ、あたらしいデータのはず
+    expect(data2).toEqual([
+      {
+        title: "test after",
+        description: "test desc after",
+        link: "http://example.com/after",
       },
     ]);
   });
